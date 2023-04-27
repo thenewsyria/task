a40b63aaee1412a4340cce0f9e9e1cf1ef2fb675
thenewsyria <96256695+thenewsyria@users.noreply.github.com>
2023-04-27 13:48:49 +0300

    Update main.dart

--- task/lib/main.dart
+++ task/lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:clarifai_flutter/clarifai_flutter.dart';
-import 'package:profanity_filter/profanity_filter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -13,78 +11,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Demo App',
-      home: MyHomePage(),
+      home: MyHomePage(
+        title: '',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _textController = TextEditingController();
-  bool _containsBadWords = false;
-  bool _containsNudity = false;
+  TextEditingController _textEditingController = TextEditingController();
+  bool _profanityDetected = false;
 
-  Future<void> _getImageAndCheckNudity() async {
-    final pickedFile =
-        await ImagePicker().getImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      final clarifai = Clarifai(apiKey: 'your_api_key');
-      final response = await clarifai.predictModel(
-        ClarifaiModel.nsfwModel,
-        File(pickedFile.path).readAsBytesSync(),
-      );
-      final concepts = response['outputs'][0]['data']['concepts'];
+  Future<void> _checkProfanity(String text) async {
+    // Replace with your own API endpoint and API key
+    final url = 'https://api.example.com/check-profanity?text=$text';
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer your_api_key',
+    };
+
+    final response = await http.get(Uri.parse(url), headers: headers);
+
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
       setState(() {
-        _containsNudity = concepts.any((concept) => concept['name'] == 'nsfw');
+        _profanityDetected = result['profanity'];
       });
     }
   }
 
-  void _checkBadWords(String value) {
-    final profanityFilter = ProfanityFilter();
-    setState(() {
-      _containsBadWords = profanityFilter.hasProfanity(value);
-    });
+  Future<void> _checkNudity() async {
+    // Show a dialog to prompt the user for an image URL
+    final imageUrl = await TextFieldAlertDialog().asyncInputDialog(
+      context,
+      title: 'Enter image URL',
+      labelText: 'Image URL',
+    );
+
+    if (imageUrl == null || imageUrl.isEmpty) return;
+
+    // Replace with your own API endpoint and API key
+    final url = 'https://api.example.com/check-nudity';
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer your_api_key',
+    };
+    final body = json.encode({'url': imageUrl});
+
+    final response =
+        await http.post(Uri.parse(url), headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
+      final nudityDetected = result['nudity'];
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title:
+                Text(nudityDetected ? 'Nudity detected' : 'No nudity detected'),
+            content: Text('The image has been analyzed.'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo App'),
+        title: Text(widget.title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextField(
-            controller: _textController,
-            onChanged: _checkBadWords,
-            decoration: InputDecoration(
-              labelText: 'Input field',
-              errorText: _containsBadWords ? 'Contains bad words' : null,
-            ),
-          ),
-          ElevatedButton(
-            onPressed: _getImageAndCheckNudity,
-            child: Text('Select Image'),
-          ),
-          if (_containsNudity)
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Image contains nudity',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _textEditingController,
+              onChanged: (value) async {
+                await _checkProfanity(value);
+              },
+              decoration: InputDecoration(
+                labelText: 'Enter text',
+                errorText: _profanityDetected ? 'Profanity detected' : null,
               ),
             ),
-        ],
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                await _checkNudity();
+              },
+              child: Text('Check nudity in image'),
+            ),
+          ],
+        ),
       ),
     );
   }
